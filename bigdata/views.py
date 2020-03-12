b55764e6f813543539a6d5134db6fe8101f844ca
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2020-03-12 10:24:56 +0700

    template tags

--- bigdata/views.py
+++ bigdata/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    context = {
+        'judul': 'Index',
+        'header': 'Selamat Datang di Index BIGDATA Kabupaten Situbondo',
+        'nav' : [
+            ['/','Home'],
+            ['/dashboard','Dashboard'],
+            ['/about','About']
+        ]
+    }
+    return render(request,'index.html',context)
 
 def about(request):
     return render(request,'about.html')
