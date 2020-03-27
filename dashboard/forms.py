a536aa50fc6c9318e67fe4e397b9d5a384aa2dec
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2020-03-27 07:17:00 +0700

    fixing form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,74 +3,6 @@ from .models import Post
 from django.conf import settings
 
 
-# class DataForm(forms.Form):
-#     nama = forms.CharField(
-#         label='Nama Lengkap',
-#         max_length=25,
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#                 'placeholder': 'Masukkan nama lengkap'
-#             }
-#         )
-#     )
-#     jenis_kelamin = forms.ChoiceField(
-#         widget=forms.RadioSelect(
-#             attrs={
-#                 'class': 'custom-control-input'
-#             }
-#         ),
-#         choices=[
-#             ('1', 'Laki-laki'),
-#             ('2', 'Perempuan'),
-#         ],
-#     )
-#     tempat_lahir = forms.CharField(
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#                 'placeholder': 'Masukkan tempat lahir'
-#             }
-#         )
-#     )
-#     tanggal_lahir = forms.DateField(
-#         localize=True,
-#         widget=forms.DateInput(
-#             format='%Y-%m-%d',
-#             attrs={
-#                 'class': 'form-control datepicker',
-#                 'placeholder': 'yyyy-mm-dd'
-#             },
-#         ),
-#         input_formats=('%Y-%m-%d',)
-#     )
-#
-#     alamat = forms.CharField(
-#         widget=forms.Textarea(
-#             {
-#                 'class': 'form-control',
-#                 'rows': '3'
-#             }
-#         )
-#     )
-#     email = forms.EmailField(
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#                 'placeholder': 'Masukkan email'
-#             }
-#         )
-#     )
-#     hp = forms.CharField(
-#         label='Nomor HP',
-#         max_length=13,
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'form-control',
-#                 'type': 'number',
-#             }
-#         )
-#     )
 class DateInput(forms.DateInput):
 
     def __init__(self, **kwargs):
@@ -82,11 +14,18 @@ class DataForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = "__all__"
+
+        labels = {
+            'nama': 'Nama Lengkap',
+            'hp': 'HP'
+        }
+
         widgets = {
             'nama': forms.TextInput(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'Masukkan Nama'
+                    'placeholder': 'Masukkan Nama',
+                    'autocomplete': 'off'
                 }
             ),
             'jenis_kelamin': forms.RadioSelect(
@@ -97,32 +36,37 @@ class DataForm(forms.ModelForm):
             'tempat_lahir': forms.TextInput(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'Masukkan tempat lahir'
+                    'placeholder': 'Masukkan tempat lahir',
+                    'autocomplete': 'off'
                 }
             ),
             'tanggal_lahir': forms.DateInput(
                 format='%Y-%m-%d',
                 attrs={
                     'class': 'form-control datepicker',
-                    'placeholder': 'yyyy-mm-dd'
+                    'placeholder': 'yyyy-mm-dd',
+                    'autocomplete': 'off'
                 },
             ),
             'email': forms.EmailInput(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'Masukkan email'
+                    'placeholder': 'Masukkan email',
+                    'autocomplete': 'off'
                 }
             ),
             'alamat': forms.TextInput(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'Masukkan tempat lahir'
+                    'placeholder': 'Masukkan tempat lahir',
+                    'autocomplete': 'off'
                 }
             ),
             'hp': forms.TextInput(
                 attrs={
                     'class': 'form-control',
-                    'placeholder': 'No. HP'
+                    'placeholder': 'No. HP',
+                    'autocomplete': 'off'
                 }
             ),
         }
